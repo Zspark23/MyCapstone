3f5a788c7ebcdb79536832591505ebf4fbc92e07
Zack Spicer <spicer.zack8@gmail.com>
2015-08-24 14:57:18 -0600

    Initial commit

---
+++ Due-DatesTests/Due_DatesTests.m
@@ -0,0 +1,40 @@
+//
+//  Due_DatesTests.m
+//  Due-DatesTests
+//
+//  Created by Zack Spicer on 8/24/15.
+//  Copyright (c) 2015 Dev Mountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Due_DatesTests : XCTestCase
+
+@end
+
+@implementation Due_DatesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
